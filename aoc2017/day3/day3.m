adc097ad17fdfcb74e0441c11d397da73005278a
Raj Singh <rajibsingh@yahoo.com>
2021-09-04 22:38:15 -0400

    day 3 progress

--- aoc2017/day3/day3.m
+++ aoc2017/day3/day3.m
@@ -4,6 +4,7 @@ int main() {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
     NSString* fileContent = [NSString stringWithContentsOfFile:@"input.txt"
                             encoding:NSUTF8StringEncoding error:nil];
+    NSMutableSet *addresses = [NSMutableSet setWithCapacity:10];
 
 
     [pool drain];
