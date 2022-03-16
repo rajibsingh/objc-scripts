cf96427b4f270288199c8f8319eb79ca8ff5dd6d
Raj Singh <rajibsingh@yahoo.com>
2022-03-16 19:55:54 -0400

    going back and reworking some old aoc2015 things, forgotten nearly everything about the fricking syntax

--- aoc2015/day1/day1-pt2.m
+++ aoc2015/day1/day1-pt2.m
@@ -5,7 +5,6 @@ int main() {
     NSString* filePath = @"input.txt";
     NSString* fileContent = [NSString stringWithContentsOfFile:filePath
                             encoding:NSUTF8StringEncoding error:nil];
-    NSLog(@"%s", fileContent);
     NSUInteger length = [fileContent length];
     int counter = 0;
     for (NSInteger i = 0; i < fileContent.length ; i++) {
@@ -15,11 +14,10 @@ int main() {
         else if (x == ')')
             counter--;
         if (counter == -1) {
-            printf("%d", i+1);
+            NSLog(@"%ld", i+1);
             break;
         }
     }
-    NSLog(@"%d ", counter);
     [pool drain];
     return 0;
 }
--- aoc2015/day1/day1.m
+++ aoc2015/day1/day1.m
@@ -5,14 +5,10 @@ int main() {
     NSString* filePath = @"input.txt";
     NSString* fileContent = [NSString stringWithContentsOfFile:filePath
                             encoding:NSUTF8StringEncoding error:nil];
-    NSLog(@"%s", fileContent);
     NSUInteger length = [fileContent length];
-    NSLog(@"length: %lu", length);
     int counter = 0;
     for (NSInteger i = 0; i < fileContent.length ; i++) {
         unichar x = [fileContent characterAtIndex:i];
-        // NSLog(@"%C ", x);
-        // NSLog(@"%s ", leftParen);
         if (x == '(')
             counter++;
         else if (x == ')')
