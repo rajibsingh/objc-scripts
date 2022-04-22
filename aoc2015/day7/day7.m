d4cd43f2aadeb98e98d41ca45e70a37ea1035066
rajibsingh <rajibsingh@yahoo.com>
2022-04-22 00:08:42 -0400

    more progress on day7, pt1

--- aoc2015/day7/day7.m
+++ aoc2015/day7/day7.m
@@ -7,7 +7,9 @@
 -(void)addToDict:(NSString*)key value:(NSInteger)value;
 -(NSInteger)getRegister:(NSString*)name;
 -(BOOL)isNumeric:(NSString*)key;
--(void)rshift:(NSString*)src shifts:(NSInteger)shifts;
+-(void)rshift:(NSString*)src shifts:(NSInteger)shifts dest:(NSString*)dest;
+-(void)lshift:(NSString*)src shifts:(NSInteger)shifts dest:(NSString*)dest;
+-(void)band:(NSString*)src1 src2:(NSString*)src2 dest:(NSString*)dest;
 -(void)print;
 
 @end
@@ -33,20 +35,54 @@ NSMutableDictionary *board;
     return [retVal integerValue];
 }
 
--(void)rshift:(NSString*)src shifts:(NSInteger)shifts {
-
+-(void)rshift:(NSString*)src shifts:(NSInteger)shifts dest:(NSString*)dest {
+    NSLog(@"running RSHIFT case: src: %@, shifts: %ld, dest: %@", src, shifts, dest);
+    NSNumber* val = [board objectForKey:src];
+    NSLog(@"retrieved %@", val);
+    NSInteger retVal = 0;
+    if (val == nil) {
+        retVal = 0;
+    } else {
+        retVal = [val integerValue] >> shifts;
+    }
+    NSLog(@"retVal: %ld", retVal);
+    [self addToDict:dest value:retVal];
 }
+
+-(void)lshift:(NSString*)src shifts:(NSInteger)shifts dest:(NSString*)dest {
+    NSLog(@"running LSHIFT case: src: %@, shifts: %ld, dest: %@", src, shifts, dest);
+    NSNumber* val = [board objectForKey:src];
+    NSLog(@"retrieved %@", val);
+    NSInteger retVal = 0;
+    if (val == nil) {
+        retVal = 0;
+    } else {
+        retVal = [val integerValue] << shifts;
+    }
+    NSLog(@"retVal: %ld", retVal);
+    [self addToDict:dest value:retVal];
+}
+
+-(void)band:(NSString*)src1 src2:(NSString*)src2 dest:(NSString*)dest {
+    NSLog(@"running bAnd case: src1: %@ src2: %@ dest: %@", src1, src2, dest);
+    NSNumber* src1val = [board objectForKey:src1];
+    NSNumber* src2val = [board objectForKey:src1];
+    NSInteger retVal = [src1val integerValue] & [src2val integerValue];
+    NSLog(@"retVal: %ld", retVal);
+    [self addToDict:dest value:retVal];
+}
+
 
 -(void)print {
     NSLog(@"board: %@", board);
 }
 
 -(BOOL)isNumeric:(NSString*)str {
-    NSLog(@"testing: %@", str);
+    // NSLog(@"testing: %@", str);
     NSUInteger len = [str length];
     for (int i = 0; i < len; i++) {
         unichar ch = [str characterAtIndex:i];
-        NSLog(@"testing %C", ch);
+        // NSLog(@"testing %C", ch);
         switch(ch) {
             case '0':
                 break;
@@ -87,33 +123,31 @@ int main() {
     for (NSString* line in lines) {
         NSLog(@"*** line: %@", line);
         NSArray* words = [line componentsSeparatedByString:(NSString *)@" "];
-        if ([words[0] isEqualTo:@"NOT"]) {
-            NSLog(@"running NOT case");
-        } else if ([circuitboard isNumeric:words[0]]) {
-            NSLog(@"running numeric case");
+        if ([words[1] isEqualTo:@"RSHIFT"]) {
+            NSString* src = words[0];
+            int shiftAmount = [words[2] integerValue];
+            NSString* dest = words[4];
+            // NSLog(@"running RSHIFT case: src: %@, shiftAmount: %d, dest: %@", src, shiftAmount, dest);
+            [circuitboard rshift:src shifts:shiftAmount dest:dest];
+        } else if ([words[1] isEqualTo:@"LSHIFT"]) {
+            NSString* src = words[0];
+            int shiftAmount = [words[2] integerValue];
+            NSString* dest = words[4];
+            [circuitboard lshift:src shifts:shiftAmount dest:dest];
+       } else if ([words[1] isEqualTo:@"AND"]) {
+            NSString* src1 = words[0];
+            NSString* src2 = words[2];
+            NSString* dest = words[4];
+            [circuitboard band:src1 src2:src2 dest:dest];
+        }
+ 
+
+        else if ([circuitboard isNumeric:words[0]]) {
+            // NSLog(@"running numeric case");
         } else {
-            NSLog(@"running the default case");
+            // NSLog(@"running the default case");
         }
         
-        // if ([words[0] isEqualTo:@"turn"]) {
-            // NSString* startCoords = words[2];
-            // NSString* endCoords = words[4];
-            // if([words[1] isEqualTo:@"on"]) {
-                // NSLog(@"turnOn %@ %@", startCoords, endCoords);
-                // [mem setBlock:startCoords endCoord:endCoords state:YES];
-            // }
-            // else if([words[1] isEqualTo:@"off"]) {
-                // NSLog(@"turnOff %@ %@", startCoords, endCoords);
-                // [mem setBlock:startCoords endCoord:endCoords state:NO];
-            // }
-        // }
-        // else if ([words[0] isEqualTo:@"toggle"]) {
-            // NSLog(@"received toggle command");
-            // NSString* startCoords = words[1];
-            // NSString* endCoords = words[3];
-            // [mem flip:startCoords endCoord:endCoords];
-        // }
-
     }
     [circuitboard print];
 
